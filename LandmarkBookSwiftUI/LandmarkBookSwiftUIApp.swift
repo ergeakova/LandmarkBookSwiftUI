8e8c247c4fe7ed0e840d8ffa805aafda6e18b442
ergeakova <ergeak@gmail.com>
2022-07-18 01:19:20 +0300

    Initial Commit

---
+++ LandmarkBookSwiftUI/LandmarkBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkBookSwiftUIApp.swift
+//  LandmarkBookSwiftUI
+//
+//  Created by Erge Gevher Akova on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
